4077f5b4de0171fd74656ce2081144324b780d71
PattexPattex <bojan.brdar.turk@gmail.com>
2022-07-20 11:37:59 +0200

    Fixed the queue message not being sent in some cases
    If the previous message's web hook token expired, the new message would not be sent and no exceptions would be thrown.

--- src/main/java/com/pattexpattex/musicgods/music/helpers/QueueManager.java
+++ src/main/java/com/pattexpattex/musicgods/music/helpers/QueueManager.java
@@ -9,15 +9,15 @@ import com.pattexpattex.musicgods.interfaces.button.objects.ButtonInterfaceFacto
 import com.pattexpattex.musicgods.music.Kvintakord;
 import com.pattexpattex.musicgods.music.audio.MusicScheduler;
 import com.pattexpattex.musicgods.util.BotEmoji;
+import com.pattexpattex.musicgods.util.OtherUtils;
+import com.pattexpattex.musicgods.util.TimeoutTimer;
 import com.pattexpattex.musicgods.util.builders.QueueBoxBuilder;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
-import net.dv8tion.jda.api.exceptions.ErrorHandler;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonStyle;
-import net.dv8tion.jda.api.requests.ErrorResponse;
 
 import javax.annotation.Nullable;
 import java.util.concurrent.TimeUnit;
@@ -30,24 +30,21 @@ import java.util.concurrent.atomic.AtomicReference;
 public class QueueManager implements ButtonInterface, Runnable {
 
     public static final int TRACK_LINE_SIZE = 32;
-    public static final long TRACK_MOVE_MILLIS = 10000L;
+    public static final long TRACK_SEEK_MILLIS = 10000L;
 
     private final ApplicationManager manager;
     private final Kvintakord kvintakord;
     private final CheckManager checkManager;
 
-    private final AtomicReference<InteractionHook> queueBox;
-    private final AtomicInteger queueBoxPage;
-    private final AtomicBoolean creatingQueueBox;
+    private final AtomicReference<InteractionHook> message = new AtomicReference<>();
+    private final AtomicInteger messagePage = new AtomicInteger();
+    private final AtomicBoolean creatingMessage = new AtomicBoolean();
+    private final TimeoutTimer timer = new TimeoutTimer(5, TimeUnit.MINUTES, () -> OtherUtils.deleteHook(message.getAndSet(null)));
 
     private QueueManager(ApplicationManager manager, Kvintakord kvintakord) {
         this.manager = manager;
         this.kvintakord = kvintakord;
         this.checkManager = kvintakord.getCheckManager();
-
-        this.queueBox = new AtomicReference<>();
-        this.queueBoxPage = new AtomicInteger();
-        this.creatingQueueBox = new AtomicBoolean();
 
         manager.getExecutorService().scheduleAtFixedRate(this, 3, 15, TimeUnit.SECONDS);
         kvintakord.subInterfaceLoaded(this);
@@ -56,47 +53,43 @@ public class QueueManager implements ButtonInterface, Runnable {
     @ButtonHandle(identifier = "kv:current.back", emoji = BotEmoji.REWIND)
     public void backButton(ButtonInteractionEvent event) {
         checkManager.fairCheck(() -> {
-            setQueueBox(event.getHook());
             kvintakord.getScheduler().forCurrentTrack(track ->
-                    track.setPosition(Math.max(0, track.getPosition()) - TRACK_MOVE_MILLIS));
+                    track.setPosition(Math.max(0, track.getPosition()) - TRACK_SEEK_MILLIS));
     
-            updateQueueMessage();
-        }, String.format("Rewind the current track for %s seconds?", TRACK_MOVE_MILLIS), event, true);
+            setAndUpdate(event.getHook());
+        }, String.format("Rewind the current track for %s seconds?", TRACK_SEEK_MILLIS), event, true);
     }
 
     @ButtonHandle(identifier = "kv:pause", emoji = BotEmoji.PLAY_PAUSE)
     public void pauseButton(ButtonInteractionEvent event) {
         checkManager.deferredCheck(() -> {
-            setQueueBox(event.getHook());
             kvintakord.getScheduler().pause();
-            updateQueueMessage();
+            setAndUpdate(event.getHook());
         }, event, true);
     }
 
     @ButtonHandle(identifier = "kv:current.forward", emoji = BotEmoji.FAST_FORWARD)
     public void forwardButton(ButtonInteractionEvent event) {
         checkManager.fairCheck(() -> {
-            setQueueBox(event.getHook());
             kvintakord.getScheduler().forCurrentTrack(track ->
-                    track.setPosition(track.getPosition() + TRACK_MOVE_MILLIS));
-    
-            updateQueueMessage();
-        }, String.format("Fast forward the current track for %s seconds?", TRACK_MOVE_MILLIS), event, true);
+                    track.setPosition(track.getPosition() + TRACK_SEEK_MILLIS));
+            
+            setAndUpdate(event.getHook());
+        }, String.format("Fast forward the current track for %s seconds?", TRACK_SEEK_MILLIS), event, true);
     }
 
     @ButtonHandle(identifier = "kv:skip", emoji = BotEmoji.NEXT_TRACK)
     public void skipButton(ButtonInteractionEvent event) {
         kvintakord.getCheckManager().fairCheck(() -> {
-            setQueueBox(event.getHook());
             kvintakord.getScheduler().skipTrack();
-            updateQueueMessage();
+            setAndUpdate(event.getHook());
         }, "Skip the current track?", event, true);
     }
 
     @ButtonHandle(identifier = "kv:stop", emoji = BotEmoji.STOP_TRACK)
     public void stopButton(ButtonInteractionEvent event) {
         checkManager.fairCheck(() -> {
-            setQueueBox(event.getHook());
+            setMessage(event.getHook());
             kvintakord.getScheduler().stop(true);
         }, "Stop playback?", event, true);
     }
@@ -104,34 +97,31 @@ public class QueueManager implements ButtonInterface, Runnable {
     @ButtonHandle(identifier = "kv:loop", emoji = BotEmoji.REPEAT)
     public void loopButton(ButtonInteractionEvent event) {
         checkManager.deferredCheck(() -> {
-            setQueueBox(event.getHook());
             kvintakord.getScheduler().incrementLoop();
-            updateQueueMessage();
+            setAndUpdate(event.getHook());
         }, event, true);
     }
 
     @ButtonHandle(identifier = "kv:shuffle", emoji = BotEmoji.ARROWS_TWISTED)
     public void shuffleButton(ButtonInteractionEvent event) {
         checkManager.fairCheck(() -> {
-            setQueueBox(event.getHook());
             kvintakord.getScheduler().toggleShuffle();
-            updateQueueMessage();
+            setAndUpdate(event.getHook());
         }, "Toggle the shuffle mode?", event, true);
     }
 
     @ButtonHandle(identifier = "kv:clear", emoji = BotEmoji.WASTEBASKET)
     public void clearButton(ButtonInteractionEvent event) {
         checkManager.fairCheck(() -> {
-            setQueueBox(event.getHook());
             kvintakord.getScheduler().emptyQueue();
-            updateQueueMessage();
+            setAndUpdate(event.getHook());
         }, "Clear the current queue?", event, true);
     }
 
     @ButtonHandle(identifier = "kv:lyrics", emoji = BotEmoji.SCROLL)
     public void lyricsButton(ButtonInteractionEvent event) {
         checkManager.deferredCheck(() -> {
-            setQueueBox(event.getHook());
+            setMessage(event.getHook());
             kvintakord.getScheduler().forCurrentTrack(track -> kvintakord.retrieveLyrics(event.getHook(), kvintakord.getLyricsHelper().buildSearchQuery(track)));
         }, event, false);
     }
@@ -140,8 +130,8 @@ public class QueueManager implements ButtonInterface, Runnable {
             emoji = BotEmoji.ARROW_BACKWARD, style = ButtonStyle.PRIMARY)
     public void prevPageButton(ButtonInteractionEvent event) {
         checkManager.deferredCheck(() -> {
-            setQueueBox(event.getHook());
-            previousQueueBoxPage();
+            setMessage(event.getHook());
+            previousPage();
         }, event, true);
     }
 
@@ -149,85 +139,87 @@ public class QueueManager implements ButtonInterface, Runnable {
             emoji = BotEmoji.ARROW_FORWARD, style = ButtonStyle.PRIMARY)
     public void nextPageButton(ButtonInteractionEvent event) {
         checkManager.deferredCheck(() -> {
-            setQueueBox(event.getHook());
-            nextQueueBoxPage();
+            setMessage(event.getHook());
+            nextPage();
         }, event, true);
     }
 
     @ButtonHandle(identifier = "kv:destroy", label = "End interaction", style = ButtonStyle.DANGER)
     public void destroyButton(ButtonInteractionEvent event) {
-        setQueueBox(event.getHook());
         event.deferEdit().queue();
-        destroyQueueMessage();
+        setMessage(event.getHook());
+        destroyMessage();
     }
 
-    public void nextQueueBoxPage() {
-        queueBoxPage.incrementAndGet();
-        updateQueueMessage();
+    public void nextPage() {
+        messagePage.incrementAndGet();
+        updateMessage();
     }
 
-    public void previousQueueBoxPage() {
-        queueBoxPage.decrementAndGet();
-        updateQueueMessage();
+    public void previousPage() {
+        messagePage.decrementAndGet();
+        updateMessage();
     }
 
-    public void setQueueBoxPage(int newPage) {
-        queueBoxPage.set(newPage);
+    public void setPage(int newPage) {
+        messagePage.set(newPage);
         //updateQueueMessage();
     }
 
-    public void setQueueBox(InteractionHook hook) {
-        queueBox.compareAndSet(null, hook);
+    public void setMessage(InteractionHook hook) {
+        message.set(hook);
     }
 
-    public void destroyQueueMessage() {
-        InteractionHook hook = queueBox.getAndSet(null);
-        queueBoxPage.set(0);
-
-        if (hook != null)
-            hook.retrieveOriginal().flatMap(Message::delete).queue();
+    public void destroyMessage() {
+        messagePage.set(0);
+        OtherUtils.deleteHook(message.getAndSet(null));
+    }
+    
+    public void setAndUpdate(InteractionHook hook) {
+        setMessage(hook);
+        updateMessage();
+        timer.reset();
     }
 
-    public void updateQueueMessage() {
-        updateQueueMessage(null);
+    public void updateMessage() {
+        updateMessage(null);
     }
 
-    public void updateQueueMessage(@Nullable InteractionHook hook) {
+    public void updateMessage(@Nullable InteractionHook hook) {
         AudioTrack track = kvintakord.getPlayer().getPlayingTrack();
 
-        if (track == null)
-            destroyQueueMessage();
+        if (track == null) {
+            destroyMessage();
+        }
         else {
             MusicScheduler scheduler = kvintakord.getScheduler();
-            InteractionHook oldHook = queueBox.get();
+            InteractionHook old = message.get();
             Message box = QueueBoxBuilder.build(track, scheduler.getQueue(), scheduler.getLoop(),
-                    scheduler.getShuffle(), scheduler.isPaused(), scheduler.getVolume(), queueBoxPage.get(), manager);
+                    scheduler.getShuffle(), scheduler.isPaused(), scheduler.getVolume(), messagePage.get(), manager);
 
             if (hook != null) {
-                if (creatingQueueBox.compareAndSet(false, true)) {
-                    hook.editOriginal(box).queue(created -> {
-                        if (oldHook != null)
-                            oldHook.deleteOriginal().queue();
-
-                        queueBox.set(hook);
-                        creatingQueueBox.set(false);
-                    }, error ->
-                            creatingQueueBox.set(false));
+                if (creatingMessage.compareAndSet(false, true)) {
+                    hook.editOriginal(box).queue(msg -> {
+                        OtherUtils.deleteHook(old);
+                        setMessage(hook);
+                    });
+    
+                    creatingMessage.set(false);
                 }
             }
-            else if (oldHook != null) {
-                oldHook.editOriginal(box).queue(null, new ErrorHandler().ignore(ErrorResponse.INVALID_WEBHOOK_TOKEN));
+            else if (old != null) {
+                if (!old.isExpired()) old.editOriginal(box).queue();
             }
         }
     }
 
     public void cleanup() {
-        destroyQueueMessage();
+        destroyMessage();
     }
 
     @Override
     public void run() {
-        updateQueueMessage();
+        updateMessage();
     }
 
     public static class Factory implements ButtonInterfaceFactory<QueueManager> {
